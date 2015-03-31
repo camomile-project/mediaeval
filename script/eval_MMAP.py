70e7d661c52eb5b47c6fe4296bafa22f68c5098e
Johann Poignant <Johann.poignant@limsi.fr>
2015-03-31 17:05:53 +0200

    changement fichier entrée

--- script/eval_MMAP.py
+++ script/eval_MMAP.py
@@ -2,7 +2,7 @@
 Eval MMAP
 
 Usage:
-  eval_MMAP.py <path_file_rank> <file_ref> <file_list_query_ranking> <size_rank>
+  eval_MMAP.py <hyp_file> <ref_file> <queries_list> <size_rank>
   eval_MMAP.py -h | --help
 """
 
@@ -10,46 +10,50 @@ from docopt import docopt
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
-    path_file_rank = arguments['<path_file_rank>']
-    file_ref = arguments['<file_ref>']
-    file_list_query_ranking = arguments['<file_list_query_ranking>']
+    hyp_file = arguments['<hyp_file>']
+    ref_file = arguments['<ref_file>']
+    queries_list = arguments['<queries_list>']
     size_rank = int(arguments['<size_rank>'])
 
-    l_file_query_per_name = {}
-    for line in open(file_list_query_ranking):
-        name, start_date, end_date, file_rank = line.split(' ')
-        l_file_query_per_name.setdefault(name, []).append(file_rank)
+    ref = {}
+    hyp = {}
+    queries = {}
+    for line in open(queries_list):
+        video, name = line[:-1].split(' ')
+        queries.setdefault(name, set([])).add(video)
+        ref.setdefault(name, {})
+        ref[name][video] = set([])
+        hyp.setdefault(name, {})
+        hyp[name][video] = set([])
 
-    dic_ref = {}
-    for line in open(file_ref):
+    for line in open(hyp_file):
+        video, name, shot, confidence = line[:-1].split(' ')
+        if name in hyp:
+            if video in hyp[name]:
+                hyp[name][video].add([confidence, shot])
+
+    for line in open(ref_file):
         video, name, shot = line[:-1]
-        dic_ref.setdefault(name, {})
-        dic_ref[name].setdefault(video, []).append(shot)
+        if name in ref:
+            if video in ref[name]:
+                ref[name][video].add(shot)
 
     MMAP = 0.0
-    for name in l_file_query_per_name:
+    for name in queries:
         MAP = 0.0
-        for f in l_file_query_per_name[name]:
+        for video in queries[name]:
             AP = 0.0
             rank = 0.0
             nb_cor = 0.0
-            for line in open(f)
-                video, shot, conf = line[:-1].split(' ')
+            for conf, shot in sorted(hyp[name][video], reverse=True)[size_rank:]:
                 rank += 1
-                if video in dic_ref[name]:
-                    if shot in dic_ref[name][video]:
-                        nb_cor+=1.0
-                        AP += nb_cor/rank
-                if rank>size_rank:
-                    break
+                if shot in dic_ref[name][video]:
+                    nb_cor+=1.0
+                    AP += nb_cor/rank
             AP/=nb_cor 
             MAP += AP
-        MAP /= len(l_file_query_per_name[name])
+        MAP /= len(queries[name])
         print 'MAP for:', name, MAP
         MMAP += MAP
-    MMAP /= len(l_file_query_per_name)
+    MMAP /= len(queries)
     print 'total MMAP:', MMAP
-
-
-
-
